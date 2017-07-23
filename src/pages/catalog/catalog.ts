664910fd58b5695fc8190ef5297912c2da9bf036
sun <hill.sun.wei@gmail.com>
2017-07-23 20:32:56 +0800

    catalog update

--- src/pages/catalog/catalog.ts
+++ src/pages/catalog/catalog.ts
@@ -1,3 +1,4 @@
+import { Http } from '@angular/http';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
@@ -13,38 +14,22 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'catalog.html',
 })
 export class CatalogPage {
-  categoryData:Array<any>;
+ topCategories:Array<any>;
   topCategory:any;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.categoryData=[ 
-      {name:"液压阀",  isSelect:true,  children:[
-	     {name:"多路换向阀",img:"assets/img/products/test.jpeg", children:[{name:"整体多路阀"},{name:"片式多路阀"}]},
-	     {name:"方向控制阀",img:"assets/img/products/test2.jpeg", children:[{name:"梭阀"},{name:"转阀"},{name:"球阀"},{name:"液压锁"},{name:"行程阀"},{name:"逻辑阀"},{name:"单向阀"},{name:"电磁球阀"},{name:"液控单向阀"},{name:"手动换向阀"},{name:"电磁换向阀"},{name:"液控换向阀"},{name:"电液换向阀"},{name:"其他换向阀"}]},
-	     {name:"流量控制阀",img:"assets/img/products/test.jpeg", children:[{name:"整体多路阀"},{name:"片式多路阀"}]},
-	     {name:"压力控制阀",img:"assets/img/products/test2.jpeg", children:[{name:"整体多路阀"},{name:"片式多路阀"}]},
-	     {name:"特殊应用阀",img:"assets/img/products/test.jpeg"},
-	     {name:"其他控制阀",img:"assets/img/products/test2.jpeg", children:[{name:"整体多路阀"},{name:"片式多路阀"}]},
-	     {name:"比例阀与伺服阀",img:"assets/img/products/test.jpeg", children:[{name:"整体多路阀"},{name:"片式多路阀"}]}
-	   ]},
-      {name:"液压泵",  isSelect:false, children:[{name:"齿轮泵",img:"assets/img/products/test.jpeg", children:[{name:"外啮合齿轮泵"},{name:"内啮合齿轮泵"},{name:"其他齿轮泵"}]}] },
-      {name:"液压马达", isSelect:false},
-      {name:"快速接头", isSelect:false},
-      {name:"阀用线圈", isSelect:false},
-      {name:"测量元件", isSelect:false},
-      {name:"液压软管", isSelect:false},
-      {name:"液压密封", isSelect:false},
-      {name:"其他元件", isSelect:false},
-     ];
-     //默认显示
-     this.topCategory = this.categoryData[0];
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http:Http) {
+   
   }
-
   ionViewDidLoad() {
-    console.log('ionViewDidLoad CatalogPage');
+     this.http.get("http://localhost:15536/api/categories").subscribe(res => {
+        var root = res.json();
+        this.topCategories = root.categories;
+        var initialItem = this.topCategories[0];
+        this.showSubCategories(initialItem)
+      })
   }
    
   showSubCategories(item: any) {
-    this.categoryData.forEach(item =>item.isSelect=false);
+    this.topCategories.forEach(item =>item.isSelect=false);
     item.isSelect=true;
     this.topCategory = item;
   }
