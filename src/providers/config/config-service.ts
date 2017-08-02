16df379cb27d7eacbdfdb7113d7200d8f5777d8c
sun <hill.sun.wei@gmail.com>
2017-08-02 09:06:34 +0800

    change api endpoint

--- src/providers/config/config-service.ts
+++ src/providers/config/config-service.ts
@@ -1,4 +1,5 @@
 
 export class Config {
-  static  API_SERVER :string ="http://localhost:15536/api";
+  // static  API_SERVER :string ="http://localhost:15536/api";
+   static  API_SERVER :string ="http://114.55.244.67/api";
 }
